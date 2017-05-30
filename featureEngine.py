a50842c81125595f3bb7134e85eed948a7c3b5d5
alexgulyi <alexander.gulyi@gmail.com>
2017-05-30 23:07:24 +0300

    Added feature engineering script

---
+++ featureEngine.py
@@ -0,0 +1,49 @@
+import numpy
+from pandas import read_csv
+from re import search, match
+
+def LoadData(path):
+	"""Loads a file into dataframe"""
+	try:
+		return(read_csv(path))
+	except OSError:
+		return(None)
+
+def GrabAttribute(attr, names):
+	"""Grab predefined attribute from name field value"""
+	if attr == 'title':		
+		pattern = "(?<=,\s)([^\.])*"
+	elif attr == 'surname':
+		pattern = "^([^,])*"
+	else:
+		return(None)
+	
+	result = []
+	for name in names:
+		try:
+			match = search(pattern, name)
+			result.append(match.group())
+		except AttributeError:
+			result.append("NONE")
+			continue
+	return(result)
+
+def MapFamilies(names):
+	"""Takes names array and returns dict with <surname>:<# of family members>"""
+	familyDict = {}
+	families = numpy.array(GrabAttribute('surname', names))
+	familyUnique = set(families)
+
+	for f in familyUnique:
+		familyDict[f] = len(numpy.where(families == f)[0])
+
+	return(familyDict)
+
+
+if __name__ == '__main__':
+	dataFrame = LoadData('train.csv')
+
+	names = dataFrame.loc[:,"Name"]
+	# columns to rebuild dataframe
+	titles = GrabAttribute('title', names)
+	familiesSize = MapFamilies(names)
