2d16dd3d1ed248d10ccadc57f11d2e6b3169a1f2
alex <alexander.gulyi@gmail.com>
2017-06-30 15:36:39 +0300

    Added classify package with sklearn.tree wrapper

---
+++ classify/tree.py
@@ -0,0 +1,36 @@
+from sklearn import tree, metrics
+from os import system
+
+class clfTree(object):
+	"""Wrapper class for sklearn.tree"""
+
+	def __init__(self, featuresNames, targetName, tune):
+		"""Creates a classifier with specified parameters in tune"""
+		self.clf = tree.DecisionTreeClassifier(splitter = 'best', **tune)
+		self.features = featuresNames
+		self.target = targetName
+		self.parameters = tune
+
+	def fit(self, trainData):
+		"""Fits classifier with trainData"""
+		X = trainData.loc[:, self.features]
+		Y = trainData.loc[:, self.target]
+		self.clf.fit(X, Y)
+
+	def draw(self):
+		"""Draws a tree in .png in current folder"""
+		tree.export_graphviz(self.clf, out_file = "tree.dot", feature_names = featureNames)
+		system('dot -Tpng tree.dot -o tree.png')
+		system('rm tree.dot')
+
+	def predict(self, testData, crossValidate = False):
+		"""Makes prediction of self.target values based on testData"""
+		X = testData.loc[:, self.features]
+		Y = self.clf.predict(X)
+		score = None
+
+		if crossValidate:
+			trueY = testData.loc[:, self.target]
+			score = metrics.accuracy_score(Y, trueY)
+
+		return(Y, score)
