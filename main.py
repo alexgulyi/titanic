8175c6622f15200d0889375a370641dc54be5124
alexgulyi <alexander.gulyi@gmail.com>
2017-07-04 23:21:07 +0300

    Added draft of stabilization function for optimizing gini coefficient value

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import classify.tree as clftree
-import classify.optimize as opt
 import data_processing.parser as prs
 import data_processing.sampler as smp
 
 if __name__ == '__main__':
 	target = "Survived"
 	features = ["Pclass", "Age", "SibSp", "Parch", "Fare", "Rank", "Sex", "Embarked"]
-	treeParameters = {'max_depth' : 10, 'min_samples_split' : 4, 'min_samples_leaf' : 10, 'min_impurity_split' : .10}
 
 	data = prs.prepareDataSet("train.csv")
-	dfTrain, dfTest = smp.sampleData(data, 0.33)	
+	dfTrain, dfTest = smp.sampleData(data, 0.33)
 	clf = clftree.clfTree(features, target)
-
-	#for x in range(1,100):
-	#	print(x / 100, opt.lossFunction(x / 100, clf, dfTest))
-	#opt.minimizeFunction(opt.lossFunction, clf, dfTest)
+	
+	def steady(tree, num = 100):
+		"""Stabilize optimization process for a arbitrary tree"""
+		solutions = []
+		for i in range(0, num - 1):
+			solutions.append(clf.optimize(dfTrain, dfTest))
+		minError = min(solutions, key = lambda x : x[1])
+		print(minError)
+		return(solutions)
+	
+	print(steady(clf, 20))
