e5acaa2251aa9923efbec504b5637fc5767d4de4
alex <alexander.gulyi@gmail.com>
2017-07-05 18:42:34 +0300

    Implemented full sequence of data processing and prediction using cross-validation

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ import data_processing.sampler as smp
 if __name__ == '__main__':
 	target = "Survived"
 	features = ["Pclass", "Age", "SibSp", "Parch", "Fare", "Rank", "Sex", "Embarked"]
+	samplingsNum = 6
+	samplingsRate = 0.5
 
 	data = prs.prepareDataSet("train.csv")
-	dfTrain, dfTest = smp.sampleData(data, 0.33)
+	testData = prs.prepareDataSet("test.csv")
+	
+	optima = []
+	for i in range(1, samplingsNum):
+		print("Sampling #{} on rate = {}:".format(i, samplingsRate))
+		# making random train subsamples
+		dfTrain, dfTest = smp.sampleData(data, samplingsRate)
+		clf = clftree.clfTree(features, target)
+		optimum = clf.fitOptimal(dfTrain, dfTest)
+		optima.append(optimum)
+		print("min(Gini) = {}\nmin(Error) = {}\n".format(*optimum))
+
+	print("Choosen Gini value: {} deliveres minimal error = {}".format(*min(optima)))
+
 	clf = clftree.clfTree(features, target)
-	
-	def steady(tree, num = 100):
-		"""Stabilize optimization process for a arbitrary tree"""
-		solutions = []
-		for i in range(0, num - 1):
-			solutions.append(clf.optimize(dfTrain, dfTest))
-		minError = min(solutions, key = lambda x : x[1])
-		print(minError)
-		return(solutions)
-	
-	print(steady(clf, 20))
+	clf.setImpurity(min(optima)[0])
+	clf.fit(data)
+	prediction = clf.predict(testData)
+
+	prs.toCSV(prs.packResult(testData.loc[:, "PassengerId"], prediction['Y']), "submission.csv")
