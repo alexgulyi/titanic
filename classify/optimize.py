09a6b85be8aad053c0446b7012282cc728e8e5af
alex <alexander.gulyi@gmail.com>
2017-07-06 17:24:28 +0300

    Small refactoring

--- classify/optimize.py
+++ classify/optimize.py
@@ -3,7 +3,9 @@ import data_processing.sampler as smp
 
 
 def getMinGini(tree, data, iterNum, sampleRate):
+	"""Computes optimal Gini coefficient for a taken tree and data, sampled by a sampleRate."""
 	optima = []
+	
 	for i in range(1, iterNum):
 		print("Sampling #{} on rate = {}:".format(i, sampleRate))
 		# making random train subsamples
@@ -12,14 +14,16 @@ def getMinGini(tree, data, iterNum, sampleRate):
 		optima.append(optimum)
 		print("min(Gini) = {}\nmin(Error) = {}\n".format(*optimum))
 	opt = min(optima)
+	
 	print("Choosen Gini value: {} deliveres minimal error = {}".format(*opt))
+	
 	return(opt)
 
-def logPredictionMetrics(clf, data, sampleRate):
+def logPredictionMetrics(tree, data, sampleRate):
 	"""Making cross-validated prediction and getting score, clf report and confusion matrix printed"""
 	dfTrain, dfTest = smp.sampleData(data, sampleRate)
-	clf.fit(dfTrain)
-	result = clf.predict(dfTest, crossValidate = True)
+	tree.fit(dfTrain)
+	result = tree.predict(dfTest, crossValidate = True)
 	print("Classification score = {}\n".format(result['score']))
 	print(metrics.classification_report(dfTest.loc[:, 'Survived'], result['Y']))
 	print("Confusion matrix:")
