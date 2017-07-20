f19b6939e2ca1b12d500495e149791b552cba44f
alex <alexander.gulyi@gmail.com>
2017-07-20 18:47:49 +0300

    Typos fixes

--- classify/optimize.py
+++ classify/optimize.py
@@ -2,14 +2,14 @@ from sklearn import metrics
 import data_processing.sampler as smp
 
 
-def getMinGini(tree, data, iterNum, sampleRate):
+def getOptGini(tree, data, iterNum, sampleRate, bootstrap = False):
 	"""Computes optimal Gini coefficient for a taken tree and data, sampled by a sampleRate."""
 	optima = []
 	
 	for i in range(1, iterNum):
 		print("Sampling #{} on rate = {}:".format(i, sampleRate))
 		# making random train subsamples
-		dfTrain, dfTest = smp.sampleData(data, sampleRate)
+		dfTrain, dfTest = smp.sampleData(data, sampleRate, intersect = bootstrap)
 		optimum = tree.fitOptimal(dfTrain, dfTest)
 		optima.append(optimum)
 		print("min(Gini) = {}\nmin(Error) = {}\n".format(*optimum))
@@ -19,9 +19,9 @@ def getMinGini(tree, data, iterNum, sampleRate):
 	
 	return(opt)
 
-def logPredictionMetrics(tree, data, sampleRate):
+def logPredictionMetrics(tree, data, sampleRate, bootstrap = False):
 	"""Making cross-validated prediction and getting score, clf report and confusion matrix printed"""
-	dfTrain, dfTest = smp.sampleData(data, sampleRate)
+	dfTrain, dfTest = smp.sampleData(data, sampleRate, intersect = bootstrap)
 	tree.fit(dfTrain)
 	result = tree.predict(dfTest, crossValidate = True)
 	print("Classification score = {}\n".format(result['score']))
