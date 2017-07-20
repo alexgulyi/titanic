96a49f37cc81adf9af0d231621a02b399b3dcec2
alex <alexander.gulyi@gmail.com>
2017-07-20 18:49:03 +0300

    Added intersection parameter

--- data_processing/sampler.py
+++ data_processing/sampler.py
@@ -1,7 +1,14 @@
 from pandas import DataFrame, concat
 
-def sampleData(dataframe, size):
-	"""Splits dataframe randomly on 2 pieces with one sized <size>, 0 <= size <= 1"""
+def sampleData(dataframe, size, intersect = False):
+	"""Splits dataframe randomly on 2 pieces with one sized <size>, 0 <= size <= 1
+	with empty intersection if intersect is True"""
 	df1 = dataframe.sample(frac = size)
-	df2 = concat([dataframe, df1]).drop_duplicates(keep = False)
+	
+	if intersect:
+		df2 = concat([dataframe, df1]).drop_duplicates(keep = False)
+	else:
+		# bootstrap-like sampling
+		df2 = dataframe.sample(frac = size)
+	
 	return(df1, df2)
