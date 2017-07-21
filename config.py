e0ae0bd6ca6eee1a34c0d68591beb94d9b664be0
alex <alexander.gulyi@gmail.com>
2017-07-21 16:13:02 +0300

    Put configurational variables into separate file

---
+++ config.py
@@ -0,0 +1,12 @@
+target = "Survived"
+features = ["Pclass",
+			"Age",
+			"SibSp",
+			"Parch",
+			"Fare",
+			"Rank",
+			"Sex",
+			"Embarked",
+			"Ticket"]
+samplingsNum = 10
+samplingsRate = 0.4
